ae03d90078d1f8a5f27b56e160145ba602581da2
pabsgoodev <pabloantoniomascena@gmail.com>
2026-05-23 13:11:12 -0300

    Adicionando middlewares

--- src/middlewares/authUsermiddleware.ts
+++ src/middlewares/authUsermiddleware.ts
@@ -1,20 +1,26 @@
-import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import 'dotenv/config';
+import { Request, Response, NextFunction } from 'express';
 
 export function authUser(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: 'Authorization header missing' });
+
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: 'Token missing'
+        });
     }
-        const token = authHeader.split(' ')[1];
 
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
-        (req as any).user = decoded;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
 
+        jwt.verify(token, process.env.JWT_SECRET!);
+
+        next();
+
+    } catch {
+
+        return res.status(401).json({
+            message: 'Invalid token'
+        });
+    }
 }
