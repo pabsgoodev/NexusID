a7d50b599b60e99305905b741fbfaea3cc739040
pabsgoodev <pabloantoniomascena@gmail.com>
2026-05-23 16:44:54 -0300

    apenas para projeto sem vercel

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,33 +1,33 @@
-// import 'reflect-metadata'
-// import { DataSource } from 'typeorm'
-// import { User } from '../entitys/userEntity'
-// import { Product } from '../entitys/productEntity'
-
-// export const AppDataSource = new DataSource({
-//     type: 'mariadb',
-//     host: 'localhost',
-//     port: 3306,
-//     username: 'root',
-//     password: 'silvablo',
-//     database: 'webpage_db',
-//     synchronize: true,
-//     logging: false,
-//     entities: [User, Product],
-//     migrations: [],
-//     subscribers: [],
-// })
-
-import { DataSource } from "typeorm";
-import { User } from "../entitys/userEntity";
-import { Product } from "../entitys/productEntity";
+import 'reflect-metadata'
+import { DataSource } from 'typeorm'
+import { User } from '../entitys/userEntity'
+import { Product } from '../entitys/productEntity'
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    url: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false 
-    },
-    synchronize: false, 
-    logging: true,
+    type: 'mariadb',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'silvablo',
+    database: 'webpage_db',
+    synchronize: true,
+    logging: false,
     entities: [User, Product],
-});
+    migrations: [],
+    subscribers: [],
+})
+
+// import { DataSource } from "typeorm";
+// import { User } from "../entitys/userEntity";
+// import { Product } from "../entitys/productEntity";
+
+// export const AppDataSource = new DataSource({
+//     type: "postgres",
+//     url: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false 
+//     },
+//     synchronize: false, 
+//     logging: true,
+//     entities: [User, Product],
+// });
